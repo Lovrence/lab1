f69c661225149778de8150e8893a186b16b868ea
Lovrence <alexandra.lavrentyeva@gmail.com>
2019-02-13 10:39:11 +0300

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,42 @@
+// сортировка элементов массива в файле
+// fname - имя файла
+void sort(const char * fname)
+{
+	// нужно открыть файл и прочитать количество элементов
+	FILE * f;
+	f = fopen(fname, "rb+");
+	int n;
+	fread(&n, sizeof(int), 1, f); // получили кол-во элементов
+	int * tmp;
+	// пока не равно количеству елементов
+	for (int i = 0; i <= n; i++)
+	{
+
+		for (int j = 0; j < n - i; j++)
+		{
+
+			int  arr;
+			int  arrNext;
+			fseek(f, sizeof(int)*(j + 1), SEEK_SET); // очередной элемент
+			fread(&arr, sizeof(int), 1, f); // считываем очередной элемент
+			fseek(f, sizeof(int)*(j + 2), SEEK_SET); // следующий после очередного
+			fread(&arrNext, sizeof(int), 1, f); // считываем очередной элемент
+			fflush(f);
+			if (arr>arrNext)
+			{
+				// правого, то меняем их местами
+				tmp = new int[1];
+				memcpy(tmp, &arr, sizeof(int));
+				arr = arrNext;
+				arrNext = *tmp;
+				fseek(f, sizeof(int)*(j + 1), SEEK_SET); // позиция первого элемента
+				fwrite(&arr, sizeof(int), 1, f);
+
+				fseek(f, sizeof(int)*(j + 2), SEEK_SET);
+				fwrite(&arrNext, sizeof(int), 1, f);
+
+			}
+		}
+	}
+	fclose(f);
+}
